63655e0479fe69015ec4502ce3f5f383690e56ff
Vishnu <develop@techvajra.com>
2025-03-16 19:11:30 -0400

    inventory service

---
+++ inventory-service/src/main/java/com/example/vk/microservices/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.vk.microservices.inventoryservice.repository;
+
+import com.example.vk.microservices.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory (quantity, sku_code)
+values (100, 'iphone 14'),
+       (100, 'pixel_8'),
+       (200,'xyz');
+
